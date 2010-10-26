d99aafe6eaf5fddfebf671f0ca7d9feb2da065f8
Pascal Hartig <phartig@weluse.de>
2010-10-26 15:29:03 +0200

    Version Bump
    Includes backward-incompatible change in hook signature.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except ImportError:
 
 setup(
     name='django-templatehooks',
-    version='0.3.1',
+    version='0.4',
     author='Pascal Hartig',
     author_email='phartig@weluse.de',
     packages=['templatehooks', 'templatehooks.templatetags'],
